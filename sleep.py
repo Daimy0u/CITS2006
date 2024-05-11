0a6e556ef8f5636b5cde554544d5271d1500fa33
TiselleWang <134193892+TiselleWang@users.noreply.github.com>
2024-05-11 11:41:15 +0800

    Add files via upload

---
+++ sleep.py
@@ -0,0 +1,39 @@
+import os
+import time
+import random
+from mtdrandom import FileEncryptorMTD
+
+"""
+# Path to the directory
+directory = "./test/hello.txt"
+FileEncryptorMTD().encrypt_file(directory)
+print("File re-encrypted:", directory)
+"""
+def encrypt_files_in_directory(directory):
+    """
+    Encrypt all files in the given directory with a different encryption method.
+    """
+    for root, dirs, files in os.walk(directory):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            print("File re-encrypting:", file_name)
+            FileEncryptorMTD().encrypt_file(file_path)
+            print("File re-encrypted:", file_name)
+def main(directory):
+    """
+    Main function to iterate over the directory and change encryption of files.
+    """
+    try:
+        while True:
+            # Encrypt files in the directory
+            encrypt_files_in_directory(directory)
+            
+            # Sleep for 10 seconds
+            print("Sleeping for 10 seconds...")
+            time.sleep(10)
+    except KeyboardInterrupt:
+        print("Script terminated by user.")
+
+if __name__ == "__main__":
+    directory = "./test"  # Replace with the directory you want to iterate over
+    main(directory)
