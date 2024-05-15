3c208ad2f606cf5b61cdb25d301177bef37ab193
aarya1302 <1302ab11@gmail.com>
2024-05-15 10:18:27 +0000

    deleting random files

--- Write.py
+++
@@ -1,7 +0,0 @@
-from MTD_Utils import *
-def write(path):
-    MTD_Utils.MTD_Decrypt(path)
-    file = open(path, "a")
-    #file.write("adding some stuff")
-    #file.close()
-write("/home/aarya/Documents/Uni/cyber/project/CITS2006/rba/something.txt")
