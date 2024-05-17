b14d2d4e8f743f9f6b9c174a342668b74c0e1b50
aarya1302 <1302ab11@gmail.com>
2024-05-17 23:43:41 +0000

    adding file-name rotation

--- MTD_Utils.py
+++ MTD_Utils.py
@@ -3,7 +3,8 @@ import logging
 import random
 from File import File  # Importing the File class from File.py
 from Hashing import *
-from MTD_Logs import *
+from MTD_Logs import * 
+#from yara_engine.Engine import *
 # Setup Logger
 
     
@@ -49,10 +50,42 @@ class MTD_Utils:
         encrypt_method(path)
         print(f"File encrypted using {method_name}.")
     @classmethod
-    def MTD_Hashing(cls, path): 
+    def MTD_Hashing(cls, path, high_security): 
         with open(path, 'rb') as afile:
             buf = afile.read()
-            hash = SHA256.hash(buf)
-            hash_logger.info(f"{hash} {path}")
+            if(high_security):
+                hash1 = SHA256.hash(buf)
+                hash_logger.info(f"{hash1} {path} SHA256")
+            hash2 = FNV1a.hash(buf)
+            hash_logger.info(f"{hash2} {path} FNV1a")
+    @classmethod
+    def MTD_Yara_Scan(cls, path, high_security): 
+        print("heres")
+        #YaraEngine.scan("/test.txt", "4fb17c0658b0949ade6c61deb8a8a8f8cf2c58c08ae7a633e0")
+    @classmethod
+    def Rotate_Folder_Name(cls, original_name):
+        #build new name 
+        today_date = datetime.now().strftime("%Y-%m-%d")
+
+        # Generate a random number (for example, between 1000 and 9999)
+        random_number = random.randint(1000, 9999)
+        print(f"Current name {original_name}")
+
+        # Construct the new folder name
+        new_folder_name = f"high_security-{random_number}"
+        folder_path, old_folder_name = os.path.split(original_name)
+        print(f"Current path: {folder_path}")
+        
+
+        new_path = os.path.join(folder_path, new_folder_name)
+        print(f"New path {new_path}")
+
+        os.rename(original_name, new_path)
+        return new_path
+
+    
+
+            
+
 
 
