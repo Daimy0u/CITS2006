217ca85be96e9d1ca61fcbd860fd34b333004620
Daimy0u <yusukematsuki123@gmail.com>
2024-05-20 23:07:50 +0800

    Fixed bugs

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ from tests import yara
 from modules.yara_engine.Engine import YaraEngine
 
 
-yara.run('./modules/lib/yara_engine/rules/base/base.yar','./modules/lib/yara_engine/test',YaraEngine)
+yara.run('./modules/yara_engine/rules/base/base.yar','./modules/yara_engine/test',YaraEngine)
