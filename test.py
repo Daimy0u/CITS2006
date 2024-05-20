410ba2283bd9fab01219a8d1dd17448528b04e58
Daimy0u <yusukematsuki123@gmail.com>
2024-05-20 23:07:04 +0800

    Fix modular import problem.

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ from tests import yara
 from modules.yara_engine.Engine import YaraEngine
 
 
-yara.run('./modules/lib/yara_engine/rules/base/base.yar','./modules/lib/yara_engine/test',)
+yara.run('./modules/lib/yara_engine/rules/base/base.yar','./modules/lib/yara_engine/test',YaraEngine)
