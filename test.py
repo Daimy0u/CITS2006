f784088e823ac2b68b8a95a97b241a198292afe8
Daimy0u <yusukematsuki123@gmail.com>
2024-05-20 19:02:16 +0800

    added tests

--- test.py
+++ test.py
@@ -1,22 +1,5 @@
 import sys
-from tests.demo import Demo
+import tests
 
-def main(arg1,arg2,arg3):
-    demo = Demo()
-    demo.yara(arg1,arg2)
-    
-    
-if __name__ == "__main__":
-    if sys.argv[1]:
-        arg1 = str(sys.argv[1])
-        if sys.argv[2]:
-            arg2 = str(sys.argv[2])
-            if sys.argv[3]:
-                arg3 = str(sys.argv[3])
-            else:
-                arg3 = None
-        else:
-            arg2 = None
-    else:
-        arg3 = None
-    main(arg1,arg2,arg3)
+
+tests.yara('./src/lib/yara_engine/rules/base/base.yar','./src/lib/yara_engine/test')
---
+++ tests/yara.py
@@ -0,0 +1,23 @@
+import os,sys
+from src.lib.hash.hash_sha256 import sha256
+from src.lib.yara_engine.Engine import YaraEngine
+
+def run(base,testDir):
+    if type(base) == YaraEngine:
+        engine = base
+    else: engine = YaraEngine(base)
+    
+    for fName in os.listdir(testDir):
+        fpath = testDir + "/" + fName
+        if os.path.isdir(fpath):
+            run(engine,fpath)
+            continue
+        print(f"Scanning {fpath}")
+        with open(fpath, 'rb') as f:  
+            fhash = sha256(f.read()).hexdigest()
+        sdata,lst = engine.scan(fpath,fhash)
+        for logStr in lst:
+            print(logStr)
+
+if __name__ == "__main__":
+    run(str(sys.argv[1]),str(sys.argv[2]))
